6465e3f3b35fe21f90aad4010574d2b9de9a5962
aryaman vir <aryamanvir95@gmail.com>
2018-02-25 02:27:58 -0500

    Facebook login done

--- app/src/main/java/com/example/dhruv/coinsense/LoginActivity.java
+++ app/src/main/java/com/example/dhruv/coinsense/LoginActivity.java
@@ -6,35 +6,61 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button login;
     private EditText user;
     private EditText pass;
-
+    private LoginButton login_fb;
     FirebaseAuth firebaseAuth;
     private ProgressDialog pd;
+    CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.setApplicationId("1996674077241804");
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
         firebaseAuth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
 
         pd = new ProgressDialog(this);
         login = (Button) findViewById(R.id.login_btn);
+        login_fb = (LoginButton) findViewById(R.id.login_facebook);
+        login_fb.setReadPermissions("email", "public_profile");
+
         user = (EditText) findViewById(R.id.login_email);
         pass = (EditText) findViewById(R.id.login_password);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void LoginUser() {
@@ -68,9 +94,56 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(), CurrencyActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void LoginfbUser() {
+        login_fb.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                // ...
+            }
+        });
+    }
     public void checkLogin(View v) {
         if (v == login) {
             LoginUser();
+        }
+    }
+
+    public void checkFacebook(View v) {
+        if (v == login_fb) {
+            LoginfbUser();
         }
     }
 }
